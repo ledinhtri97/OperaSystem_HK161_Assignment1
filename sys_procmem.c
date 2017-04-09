8b3cb32bbbc9a82ebe923625677d124b8b4b28df
Le Dinh Tri <ledinhtri.cse.hcmut@gmail.com>
2017-04-09 10:30:13 -0700

    sys_procmem

--- sys_procmem.c
+++ sys_procmem.c
@@ -1,5 +1,6 @@
 #include <linux/linkage.h>
 #include <linux/sched.h>
+#include <linux/kernel.h>
 
 struct proc_segs {
 	unsigned long mssv;
@@ -14,23 +15,20 @@ struct proc_segs {
 
 asmlinkage long sys_procmem(int pid, struct proc_segs * info){
 	struct task_struct * task;
+	printk("Finding...\n");
 	for_each_process(task) {
 		if(task->pid == pid) {
 			info->mssv=1513656;
-			info->start_code	=		task->mm->start_code;
-			info->end_code		=		task->mm->end_code;
-			info->start_data	=		task->mm->start_data;
-			info->end_data		=		task->mm->end_data;
-			info->start_heap	=		task->mm->start_sbrk;
-			info->end_heap		=		task->mm->brk;
-			info->start_stack	=		task->mm->start_stack;
-			printk("\nMy MSSV = %d", 1513656);
-			printk("Code Segment start = %lu, end = %lu\n", task->mm->start_code, task->mm->end_code);
-			printk("Data Segment start = %lu, end = %lu\n", task->mm->start_data, task->mm->end_data);
-			printk("Heap Segment start = %lu, end = %lu\n", task->mm->start_heap, task->mm->end_heap);
-			printk("Stack Segment start = %lu\n", task->mm->start_stack);
-			break;
+			info->start_code = task->mm->start_code;
+			info->end_code = task->mm->end_code;
+			info->start_data = task->mm->start_data;
+			info->end_data = task->mm->end_data;
+			info->start_heap = task->mm->start_brk;
+			info->end_heap = task->mm->brk;
+			info->start_stack = task->mm->start_stack;
+			printk("Find out pid [%d]", pid);
+			return 0;
 		}
 	}
-	return 0;
+	return -1;
 }
